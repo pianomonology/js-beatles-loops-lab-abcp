18f932ba532170176783a5a0ee40e2c0a2feba51
Snel <snelson10@sfccmo.edu>
2018-02-04 17:26:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function johnLennonFacts(facts){
 
 function iLoveTheBeatles(number){
   var array =[];
-  var count = 0;
+  var count = 1;
   do{
     array.push('I love the Beatles!');
     count++;
