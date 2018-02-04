efe68c452bcafefd70883ab5863b2853542bbb7e
Snel <snelson10@sfccmo.edu>
2018-02-04 16:34:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function theBeatlesPlay (musicians, instruments){
+  var text = [];
+  
+  for (var i = 0; i < musicians.length; i++){
+    
+    text.push(musicians[i]+ ` plays ` + instruments[i]);
+  }
+  return text;
+}
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts){
+  var count = 0;
+  while (count < facts.length){
+    facts[count] = facts[count] + '!!!';
+    count++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(number){
+  var array;
+  do{
+    array.push('I love the Beatles!');
+    number++;
+  } while (number < 15);
+  return array;
+}
